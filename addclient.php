d8b7c5fd4923859ba0b2db83f14ce13fef2ffadc
LARGEAUD <alexandre.largeaud.externe@sogeti.com>
2019-09-17 14:07:19 +0200

    majajout

--- addclient.php
+++ addclient.php
@@ -1,4 +1,24 @@
 <?php
-    echo $_POST['name'] . "<br>";
-    echo $_POST['dn'] ;
-    ?>
+$servername = "db:3306";
+$username = "root";
+$password = "Azerty01";
+$dbname = "gestioncompte";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$nom = $_POST['nom'] ;
+$prenom = $_POST['prenom'] ;
+$dn = $_POST['dn'] ;
+$sql = "INSERT INTO clients (nom, prenom, datenaissance) VALUES (' $nom ', ' $prenom ', ' $dn ')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
